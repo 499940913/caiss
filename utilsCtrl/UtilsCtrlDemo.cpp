5e61fa75d672e19a756a04e64b7a57b823d7068f
junfeng.fj <junfeng.fj@alibaba-inc.com>
2020-07-08 22:50:42 +0800

    temp add

--- utilsCtrl/UtilsCtrlDemo.cpp
+++ utilsCtrl/UtilsCtrlDemo.cpp
@@ -5,36 +5,49 @@
 #include <iostream>
 #include <string>
 #include "UtilsInclude.h"
+#include <omp.h>
+#include <vector>
+#include <chrono>
 
 using namespace std;
 
-int demo_Trie () {
-    auto* trie = new TrieProc();
 
-    string words[] = {"ab"};
-    //string words[4] = {"ab"};
-    for (auto &word : words) {
-        trie->insert(word);
+CAISS_RET_TYPE normalizeNode(std::vector<CAISS_FLOAT>& node) {
+
+    int dim = node.size();
+
+    CAISS_FLOAT sum = 0.0;
+    for (unsigned int i = 0; i < dim; i++) {
+        sum += (node[i] * node[i]);
     }
 
-    bool ret = trie->find("ab");
-    cout << ret << endl;
+    CAISS_FLOAT denominator = std::sqrt(sum);    // 分母信息
+    for (unsigned int i = 0; i < dim; i++) {
+        node[i] = node[i] / denominator;
+    }
 
-    trie->clear();
-    delete trie;
-
-    return 0;
-}
-
-int demo_EditDistanceProc() {
-    string a = "help";
-    string b = "helapp";
-    int i = EditDistanceProc::calc(a, b);
-    cout << i << endl;
-    return 0;
+    return CAISS_RET_OK;
 }
 
 int main() {
-    demo_EditDistanceProc();
+    vector<float> vec1;
+    vector<float> vec2;
+
+    for (int i = 0; i < 768; i++) {
+        vec1.push_back(rand());
+        vec2.push_back(rand());
+    }
+
+    normalizeNode(vec1);
+    normalizeNode(vec2);
+
+    int dim = vec1.size();
+    float sum = 0;
+#pragma omp parallel for num_threads(4) reduction(+:sum)
+    for (int i = 0; i < dim ; i++) {
+        sum = vec1[i] * vec2[i];
+    }
+
+    cout << 1 - sum << endl;
     return 0;
 }
